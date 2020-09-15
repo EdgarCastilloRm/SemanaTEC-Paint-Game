abd139a9a64a2dc9154889d715d5706c84427635
EdgarCastilloRm <69534609+EdgarCastilloRm@users.noreply.github.com>
2020-09-15 10:30:47 -0500

    Inicial (código pintando)
    Se actualiza al código correcto que se nos brindó desde la página de Freegames.

--- Inicial.py
+++ Inicial.py
@@ -1,22 +1,68 @@
-#matricula y nombre 1
-#matricula y nombre 2
-#proposito del programa
+from turtle import *
+from freegames import vector
 
-#funcion1  calcular área del rectángulo
+def line(start, end):
+    "Draw line from start to end."
+    up()
+    goto(start.x, start.y)
+    down()
+    goto(end.x, end.y)
 
+def square(start, end):
+    "Draw square from start to end."
+    up()
+    goto(start.x, start.y)
+    down()
+    begin_fill()
 
-#funcion2  calcular perímetro del rectángulo
+    for count in range(4):
+        forward(end.x - start.x)
+        left(90)
 
+    end_fill()
 
-#instrucciones de accion
-#pedir datos
-print("medida de un lado del rectángulo")
-l1 = float(input())
+def circle(start, end):
+    "Draw circle from start to end."
+    pass  # TODO
 
-print("medida de un lado del rectángulo")
-l2 = float(input())
+def rectangle(start, end):
+    "Draw rectangle from start to end."
+    pass  # TODO
 
-#desplegar calculo funcion1
+def triangle(start, end):
+    "Draw triangle from start to end."
+    pass  # TODO
 
-#desplegar calculo funcion 2
+def tap(x, y):
+    "Store starting point or draw shape."
+    start = state['start']
+
+    if start is None:
+        state['start'] = vector(x, y)
+    else:
+        shape = state['shape']
+        end = vector(x, y)
+        shape(start, end)
+        state['start'] = None
+
+def store(key, value):
+    "Store value in state at key."
+    state[key] = value
+
+state = {'start': None, 'shape': line}
+setup(420, 420, 370, 0)
+onscreenclick(tap)
+listen()
+onkey(undo, 'u')
+onkey(lambda: color('black'), 'K')
+onkey(lambda: color('white'), 'W')
+onkey(lambda: color('green'), 'G')
+onkey(lambda: color('blue'), 'B')
+onkey(lambda: color('red'), 'R')
+onkey(lambda: store('shape', line), 'l')
+onkey(lambda: store('shape', square), 's')
+onkey(lambda: store('shape', circle), 'c')
+onkey(lambda: store('shape', rectangle), 'r')
+onkey(lambda: store('shape', triangle), 't')
+done()
 
